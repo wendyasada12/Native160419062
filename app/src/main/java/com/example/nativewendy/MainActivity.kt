814a3ed587029e86b7767424089294769aa5b21a
wendyasada12 <wendyasada4@gmail.com>
2022-02-09 12:28:23 +0800

    init

--- app/src/main/java/com/example/nativewendy/MainActivity.kt
+++ app/src/main/java/com/example/nativewendy/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //test
+    var login:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
